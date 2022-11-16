4a66f4a0eae34b45e916e789df7f327a72e87ab1
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2022-11-16 10:57:08 -0600

    push for discussion ticket

--- levelupapi/models/__init__.py
+++ levelupapi/models/__init__.py
@@ -1,1 +1,5 @@
 from .gamer import Gamer
+from .event import Event
+from .event_gamer import EventGamer
+from .game_type import GameType
+from .game import Game
--- levelupapi/models/gamer.py
+++ levelupapi/models/gamer.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Gamer(models.Model):
-
     uid = models.CharField(max_length=50)
     bio = models.CharField(max_length=50)
+    
