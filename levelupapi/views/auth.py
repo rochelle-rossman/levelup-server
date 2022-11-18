72998486fc846d620595ead0bd44cdb785b446bd
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2022-11-18 11:39:54 -0600

    tried implementing changes from Brett & Gavin. unsuccessful.

--- levelupapi/views/auth.py
+++ levelupapi/views/auth.py
@@ -10,21 +10,22 @@ def check_user(request):
     Method arguments:
       request -- The full HTTP request object
     '''
+    
     uid = request.data['uid']
 
     # Use the built-in authenticate method to verify
     # authenticate returns the user object or None if no user is found
-    gamer = Gamer.objects.get(uid=uid)
+    try:
+        gamer = Gamer.objects.get(uid=uid)
 
     # If authentication was successful, respond with their token
-    if gamer is not None:
         data = {
             'id': gamer.id,
             'uid': gamer.uid,
             'bio': gamer.bio
         }
         return Response(data)
-    else:
+    except:
         # Bad login details were provided. So we can't log the user in.
         data = { 'valid': False }
         return Response(data)
@@ -40,7 +41,7 @@ def register_user(request):
     # Now save the user info in the levelupapi_gamer table
     gamer = Gamer.objects.create(
         bio=request.data['bio'],
-        user=request.data['uid']
+        uid=request.data['uid']
     )
 
     # Return the gamer info to the client
